186ea2f73ba7a83bc82d2de610021f03f53b76ed
minjie <minjie@mjMacBook-Pro.local>
2019-10-11 16:13:56 +0800

    小调整

--- src/containers/pages/login/Login.tsx
+++ src/containers/pages/login/Login.tsx
@@ -24,7 +24,6 @@ export default class Login extends RootComponent<LoginProps, LoginState> {
   render () {
     return (
       <div className="login-content" >
-        sss
       </div>
     )
   }
---
+++ src/router/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * @authors minjie
+ * @date    2019/10/11
+ * @version 1.0.0 firstVersion
+ * @module axios
+ * @description axios 进行业务的实现
+ * @copyright Copyright © 2019 Shanghai Yejia Digital Technology Co., Ltd. All rights reserved.
+ */
+
+import * as React from 'react'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+
+export default class RootRouter {
+}
